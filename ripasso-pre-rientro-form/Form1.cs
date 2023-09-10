b6b2a8afc113dd30fb11d419e29b1a6d2d619545
Alessandro Caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-09-10 15:50:04 +0200

    Aggiungere i file di progetto.

---
+++ ripasso-pre-rientro-form/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ripasso_pre_rientro_form
+{
+	public partial class Form1 : Form
+	{
+		public string path = @"../../caslini.csv";
+		public int l = 0;
+
+		Funzioniesterne a;
+		public  Form1()
+		{
+			InitializeComponent();
+			a = new Funzioniesterne();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
