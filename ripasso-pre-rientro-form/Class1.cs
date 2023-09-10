d404f3424d48e568f6e7f2fe34eddab186a793bf
Alessandro Caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-09-10 17:48:23 +0200

    Implementazione funzioni + bottoni e textbox

--- ripasso-pre-rientro-form/Class1.cs
+++ ripasso-pre-rientro-form/Class1.cs
@@ -1,13 +1,167 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ripasso_pre_rientro_form
 {
+	
 	internal class Funzioniesterne
 	{
+		public string path = @"..\..\..\file\caslini.csv";
+		
+
+		//funzione
+		public void miovalore(char sep = ';')
+		{
+			string[] linea = File.ReadAllLines(path);
+			if (File.Exists(path))
+			{
+				using (StreamWriter sw = new StreamWriter(path))
+				{
+					Random r = new Random();
+					linea[0] += ";miovalore";
+					sw.WriteLine(linea[0]);
+					for (int i = 1; i < linea.Length; i++)
+					{
+						linea[i] += sep + r.Next(10, 21).ToString();
+						sw.WriteLine(linea[i]);
+					}
+				}
+			}
+		}
+
+		//funzione
+
+		public int Contacampi()
+		{
+
+			using (StreamReader sw = new StreamReader(path))
+			{
+				string nc = sw.ReadLine();
+
+				string[] campi = nc.Split(';');
+
+				int y = campi.Length;
+
+				return y;
+			}
+
+		}
+
+		//funzione
+
+		public void LunghezzaFIX(int lung)
+		{
+
+			int[] c = new int[1000];
+
+			string[] c2 = new string[1000];
+
+			int dim = 0;
+
+			using (StreamReader sw = new StreamReader(path))
+			{
+				string a;
+
+				a = sw.ReadLine();
+
+				while (a != null)
+				{
+					int b = a.Length;
+
+					c[dim] = lung - b;
+
+					c2[dim] = a;
+
+					dim++;
+
+					a = sw.ReadLine();
+				}
+
+			}
+
+			using (StreamWriter sw = new StreamWriter(path))
+			{
+				dim = 0;
+
+				while (c2[dim] != null)
+				{
+
+					string a = null;
+
+					for (int j = 0; j < c[dim]; j++)
+					{
+						a = a + " ";
+					}
+
+					sw.WriteLine(c2[dim] + a);
+
+					dim++;
+				}
+			}
+
+		}
+
+		//funzione
+
+		public void Aggiuntarecord(string coda1, string coda2, string coda3)
+		{
+			bool[] a = new bool[1000];
+
+			string[] p = new string[1000];
+
+			int dim = 0;
+
+			using (StreamReader sw = new StreamReader(path))
+			{
+				string b = sw.ReadLine();
+
+				while (b != null)
+				{
+					a[dim] = true;
+
+					p[dim] = b;
+
+					b = sw.ReadLine();
+
+					dim++;
+				}
+
+				if (b == null)
+				{
+					a[dim] = false;
+				}
+
+			}
+
+			using (StreamWriter sw = new StreamWriter(path))
+			{
+				dim = 0;
+
+				while (dim < 1000)
+				{
+
+					if (a[dim] == false)
+					{
+						sw.WriteLine(coda1 + ";" + coda2 + ";" + coda3 + ";");
+						break;
+					}
+
+					sw.WriteLine(p[dim]);
+
+					dim++;
+				}
+			}
+		}
+		//funzione
+		
+
 
 	}
+
 }
+
+
--- ripasso-pre-rientro-form/Form1.cs
+++ ripasso-pre-rientro-form/Form1.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ripasso_pre_rientro_form
 {
 	public partial class Form1 : Form
 	{
-		public string path = @"../../caslini.csv";
+		public string path = @"..\..\..\file\caslini.csv";
 		public int l = 0;
 
 		Funzioniesterne a;
@@ -26,5 +28,113 @@ namespace ripasso_pre_rientro_form
 		{ 
 
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			a.miovalore();
+		}
+		private void button2_Click(object sender, EventArgs e)
+		{
+			a.Contacampi();
+
+			int nc = a.Contacampi();
+
+			MessageBox.Show("Il numero dei campi è:" + nc);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			using (StreamReader sw = new StreamReader(path))
+			{
+				int d = 0;
+
+				string a = sw.ReadLine();
+
+				string[] campi = a.Split(';');
+
+				int[] arr = new int[(campi.Length) + 1];
+
+				for (int i = 0; i < campi.Length; i++)
+				{
+					arr[d] = campi[i].Length;
+					d++;
+				}
+				arr[(arr.Length) - 1] = a.Length;
+
+				while (a != null)
+				{
+					d= 0;
+
+					string[] campi2 = a.Split(';');
+
+					for (int i = 0; i < campi2.Length; i++)
+					{
+						if (arr[d] < campi2[i].Length)
+						{
+							arr[d] = campi2[i].Length;
+						}
+
+						d++;
+					}
+
+					if (arr[(arr.Length) - 1] < a.Length)
+					{
+						arr[(arr.Length) - 1] = a.Length;
+					}
+
+					a = sw.ReadLine();
+
+				}
+
+				d = 0;
+
+				
+
+				for (int i = 0; i < arr.Length; i++)
+				{
+					if (i != arr.Length - 1)
+					{
+					  MessageBox.Show("Lunghezza campo " + d.ToString() + ": " + arr[i]);
+					}
+					else
+					{
+						MessageBox.Show("Lunghezza record " + d.ToString() + ": " + (arr[arr.Length - 1] + 1));
+					}
+					d++;
+				}
+
+				l = arr[arr.Length - 1];
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if (l == 0)
+			{
+
+				MessageBox.Show("Calcolare prima lunghezza del record più lungo ");
+			}
+			else
+			{
+				
+				a.LunghezzaFIX(l);
+				MessageBox.Show("Tutti i record hanno la stessa lunghezza");
+			}
+
+
+		}
+		private void button5_Click(object sender, EventArgs e)
+		{
+			a.Aggiuntarecord(textBox1.Text, textBox2.Text, textBox3.Text);
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+
+		}
+
+
+
+
+
 	}
 }
