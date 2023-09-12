7c28bf5f4388f4bf724a6792ef0b4fe0393ce642
Alessandro Caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-09-12 20:47:24 +0200

    .

--- ripasso-pre-rientro-form/Form1.Designer.cs
+++ ripasso-pre-rientro-form/Form1.Designer.cs
@@ -137,7 +137,7 @@
 			this.button6.Name = "button6";
 			this.button6.Size = new System.Drawing.Size(317, 23);
 			this.button6.TabIndex = 8;
-			this.button6.Text = "button6";
+			this.button6.Text = "VISUALIZZA";
 			this.button6.UseVisualStyleBackColor = true;
 			this.button6.Click += new System.EventHandler(this.button6_Click);
 			// 
